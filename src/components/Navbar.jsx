8858ae1eb4eba88df2dd984f53a0f8477ddd5241
Bhaskar Patel <120586781+Bhaskar-Patel-26@users.noreply.github.com>
2025-01-16 20:54:09 +0530

    Posts page is completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,9 +28,14 @@ const Navbar = () => {
             <Link to="/posts" onClick={()=> setOpenMenu(false)}>Trending</Link>
             <Link to="/posts" onClick={()=> setOpenMenu(false)}>Most Popular</Link>
             <Link to="/about" onClick={()=> setOpenMenu(false)}>About</Link>
-            <Link to="/login" onClick={()=> setOpenMenu(false)}>
+            <SignedOut>
+              <Link to="/login" onClick={()=> setOpenMenu(false)}>
                 <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login 👋</button>
-            </Link>
+              </Link>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
         </div>
       </div>
 
